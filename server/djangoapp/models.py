241feac46cf3b0b4521febe85f55faa835bcbc79
Gustavs Fricis Jankavs <gfjankavs@tutamail.com>
2026-09-17 09:04:42 -0400

    add Django CarModel and CarMake models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,12 +1,19 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(default=now)
 
+    def __str__(self):
+        return self.name
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
 # - Description
@@ -23,3 +30,30 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'SEDAN'
+    SUV = 'SUV'
+    WAGON = 'WAGON'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+    ]
+
+    # Many-to-one: one CarMake has many CarModels
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    # Refers to a dealer stored in the Cloudant/MongoDB database
+    dealer_id = models.IntegerField(default=0)
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default=SUV)
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015),
+        ],
+    )
+    created_at = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"
